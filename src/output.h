52fe96130ae4fe39a7594acac77229bff07e230e
Parfenov Sergey <grey1105@yandex.ru>
2019-12-13 00:54:44 +0500

    Update output.h

--- src/output.h
+++ src/output.h
@@ -19,27 +19,27 @@ void output_results(molModel *mod, const string & filename) 	// output results o
 	fclose(fout);
 
 	fout = fopen("Results/tau.txt", "w");
-	fprintf(fout, "#id \tup->low \tfreq., GHz \ttau \tExcitation temperature\n");
+	fprintf(fout, "#id \t up->low \t freq., GHz \t tau \t Excitation temperature\n");
 	for (size_t i = 0; i < mod->rad_trans.size(); i++) {
-		fprintf(fout, "%ld %ld -> %ld\t\t", mod->rad_trans[i].trans_id, mod->rad_trans[i].up_level+1, mod->rad_trans[i].low_level+1);
+		fprintf(fout, "%ld \t %ld -> %ld \t ", mod->rad_trans[i].trans_id, mod->rad_trans[i].up_level+1, mod->rad_trans[i].low_level+1);
 		fprintf(fout, "%le \t %le \t %le\n", mod->rad_trans[i].nu * 1.e-9, mod->rad_trans[i].tau, mod->rad_trans[i].Tex);
 	}
 	fclose(fout);
 
 	fout = fopen("Results/Tb.txt", "w");
-	fprintf(fout, "#id \tup->low \tfreq., GHz \tBrightness temperature\n");
+	fprintf(fout, "#id \t up->low \t freq., GHz \t Brightness temperature\n");
 	for (size_t i = 0; i < mod->rad_trans.size(); i++) {
-		fprintf(fout, "%ld %ld -> %ld\t\t", mod->rad_trans[i].trans_id, mod->rad_trans[i].up_level+1, mod->rad_trans[i].low_level+1);
+		fprintf(fout, "%ld \t %ld -> %ld \t ", mod->rad_trans[i].trans_id, mod->rad_trans[i].up_level+1, mod->rad_trans[i].low_level+1);
 		fprintf(fout, "%le \t %le\n", mod->rad_trans[i].nu * 1.e-9, mod->rad_trans[i].Tbr);
 	}
 	fclose(fout);
 
 	fout = fopen("Results/masers.txt", "w");
-	fprintf(fout, "#id \tup->low \tfreq., GHz \ttau \tBrightness temperature \tExcitation temperature\n");
+	fprintf(fout, "#id \t up->low \t freq., GHz \t tau \t Brightness temperature \t Excitation temperature\n");
 	for (size_t i = 0; i < mod->rad_trans.size(); i++) {
 		if (mod->rad_trans[i].tau < -0.01) {
-			fprintf(fout, "%ld\t%ld -> %ld\t", mod->rad_trans[i].trans_id, mod->rad_trans[i].up_level + 1, mod->rad_trans[i].low_level + 1);
-			fprintf(fout, "%le \t %le \t", mod->rad_trans[i].nu * 1.e-9, mod->rad_trans[i].tau);
+			fprintf(fout, "%ld \t %ld -> %ld \t ", mod->rad_trans[i].trans_id, mod->rad_trans[i].up_level + 1, mod->rad_trans[i].low_level + 1);
+			fprintf(fout, "%le \t %le \t ", mod->rad_trans[i].nu * 1.e-9, mod->rad_trans[i].tau);
 			fprintf(fout, "%le \t %le\n", mod->rad_trans[i].Tbr, mod->rad_trans[i].Tex);
 		}
 	}
