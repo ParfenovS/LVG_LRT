3d415ab9aeb5be8c3747e6ca3fd8f0193590f547
Parfenov Sergey <grey1105@yandex.ru>
2021-09-03 13:26:07 +0500

    Update output.h

--- src/output.h
+++ src/output.h
@@ -2,7 +2,7 @@
 #include "molModel.h"
 #include <stdio.h>
 
-void output_results(molModel *mod, const string & filename) 	// output results of radiative transfer calculations into files
+void output_results(molModel *mod, string filename) 	// output results of radiative transfer calculations into files
 {
 	FILE *fout;
 
@@ -14,36 +14,40 @@ void output_results(molModel *mod, const string & filename) 	// output results o
 		fclose(fout);
 	}
 
-	fout = fopen("Results/J_emission.txt", "w");
+	if (mod->idspec == 0) fout = fopen("Results/J_emission.txt", "w");
+	else fout = fopen("Results/J_emission.txt", "a");
 	if (fout == NULL) throw runtime_error("can't open file to save mean intensity");
 	for (size_t i = 0; i < mod->rad_trans.size(); i++)
-		fprintf(fout, "%ld %ld\t%.17e\n", mod->rad_trans[i].up_level+1, mod->rad_trans[i].low_level+1, mod->rad_trans[i].J);
+		fprintf(fout, "%ld %ld %ld\t%.17e\n", mod->idspec+1, mod->rad_trans[i].up_level+1, mod->rad_trans[i].low_level+1, mod->rad_trans[i].J);
 	fclose(fout);
 
-	fout = fopen("Results/tau.txt", "w");
+	if (mod->idspec == 0) fout = fopen("Results/tau.txt", "w");
+	else fout = fopen("Results/tau.txt", "a");
 	if (fout == NULL) throw runtime_error("can't open file to save optical depths");
-	fprintf(fout, "#id \t up->low \t freq., GHz \t tau \t Excitation temperature\n");
+	if (mod->idspec == 0) fprintf(fout, "#molid \t transid \t up->low \t freq., GHz \t tau \t Excitation temperature\n");
 	for (size_t i = 0; i < mod->rad_trans.size(); i++) {
-		fprintf(fout, "%ld \t %ld -> %ld \t ", mod->rad_trans[i].trans_id, mod->rad_trans[i].up_level+1, mod->rad_trans[i].low_level+1);
+		fprintf(fout, "%ld \t %ld \t %ld -> %ld \t ", mod->idspec+1, mod->rad_trans[i].trans_id, mod->rad_trans[i].up_level+1, mod->rad_trans[i].low_level+1);
 		fprintf(fout, "%le \t %le \t %le\n", mod->rad_trans[i].nu * 1.e-9, mod->rad_trans[i].tau, mod->rad_trans[i].Tex);
 	}
 	fclose(fout);
 
-	fout = fopen("Results/Tb.txt", "w");
+	if (mod->idspec == 0) fout = fopen("Results/Tb.txt", "w");
+	else fout = fopen("Results/Tb.txt", "a");
 	if (fout == NULL) throw runtime_error("can't open file to save brightness temperature");
-	fprintf(fout, "#id \t up->low \t freq., GHz \t Brightness temperature\n");
+	if (mod->idspec == 0) fprintf(fout, "#molid \t transid \t up->low \t freq., GHz \t Brightness temperature\n");
 	for (size_t i = 0; i < mod->rad_trans.size(); i++) {
-		fprintf(fout, "%ld \t %ld -> %ld \t ", mod->rad_trans[i].trans_id, mod->rad_trans[i].up_level+1, mod->rad_trans[i].low_level+1);
+		fprintf(fout, "%ld \t %ld \t %ld -> %ld \t ", mod->idspec+1, mod->rad_trans[i].trans_id, mod->rad_trans[i].up_level+1, mod->rad_trans[i].low_level+1);
 		fprintf(fout, "%le \t %le\n", mod->rad_trans[i].nu * 1.e-9, mod->rad_trans[i].Tbr);
 	}
 	fclose(fout);
 
-	fout = fopen("Results/masers.txt", "w");
+	if (mod->idspec == 0) fout = fopen("Results/masers.txt", "w");
+	else fout = fopen("Results/masers.txt", "a");
 	if (fout == NULL) throw runtime_error("can't open file to save maser transitions");
-	fprintf(fout, "#id \t up->low \t freq., GHz \t tau \t Brightness temperature \t Excitation temperature\n");
+	if (mod->idspec == 0) fprintf(fout, "#molid \t transid \t up->low \t freq., GHz \t tau \t Brightness temperature \t Excitation temperature\n");
 	for (size_t i = 0; i < mod->rad_trans.size(); i++) {
 		if (mod->rad_trans[i].tau < -0.01) {
-			fprintf(fout, "%ld \t %ld -> %ld \t ", mod->rad_trans[i].trans_id, mod->rad_trans[i].up_level + 1, mod->rad_trans[i].low_level + 1);
+			fprintf(fout, "%ld \t %ld \t %ld -> %ld \t ", mod->idspec+1, mod->rad_trans[i].trans_id, mod->rad_trans[i].up_level + 1, mod->rad_trans[i].low_level + 1);
 			fprintf(fout, "%le \t %le \t ", mod->rad_trans[i].nu * 1.e-9, mod->rad_trans[i].tau);
 			fprintf(fout, "%le \t %le\n", mod->rad_trans[i].Tbr, mod->rad_trans[i].Tex);
 		}
@@ -51,11 +55,11 @@ void output_results(molModel *mod, const string & filename) 	// output results o
 	fclose(fout);
 }
 
-void output_results(molModel *mod, const string & filename, ostream & cout) 	// output results of radiative transfer calculations into console output
+void output_results(molModel *mod, string filename, ostream & cout) 	// output results of radiative transfer calculations into console output
 {
-	cout << "#id \tup->low \tfreq., GHz \ttau \tBrightness temperature \tExcitation temperature\n";
+	if (mod->idspec == 0) cout << "#molid \t transid \tup->low \tfreq., GHz \ttau \tBrightness temperature \tExcitation temperature\n";
 	for (size_t i = 0; i < mod->rad_trans.size(); i++) {
-		cout << mod->rad_trans[i].trans_id << "\t" << mod->rad_trans[i].up_level + 1 << "->" << mod->rad_trans[i].low_level + 1 << "\t\t";
+		cout << mod->idspec+1 << "\t" << mod->rad_trans[i].trans_id << "\t" << mod->rad_trans[i].up_level + 1 << "->" << mod->rad_trans[i].low_level + 1 << "\t\t";
 		cout << mod->rad_trans[i].nu * 1.e-9 << "\t\t" << mod->rad_trans[i].tau << "\t\t";
 		cout << mod->rad_trans[i].Tbr << "\t\t" << mod->rad_trans[i].Tex << endl;
 	}
