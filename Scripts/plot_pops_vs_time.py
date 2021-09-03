209b6cbd067595d744d5779092de1c6d7096d4da
Parfenov Sergey <grey1105@yandex.ru>
2021-09-03 13:44:03 +0500

    Update plot_pops_vs_time.py

--- Scripts/plot_pops_vs_time.py
+++ Scripts/plot_pops_vs_time.py
@@ -5,20 +5,51 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-#f = open(sys.argv[1])
-f = open('pops_vs_time.bin')
-num_of_levs = np.fromfile(f, dtype=np.dtype('u8'), count=1)
-num_of_levs = int(num_of_levs[0])
-rectype = np.dtype( [ ('time', 'd'), ('pops', 'd', num_of_levs) ] )
-recs = np.fromfile(f, dtype=rectype, offset=8)
+class data_class:
+	def _init(self):
+		self._num_of_species = 0
+		self._num_of_trans = []
+		self._recs = []
 
-timestep = []
-for i in range(1, len(recs['time'])):
-	timestep.append( recs['time'][i] - recs['time'][i-1])
-timestep = np.array(timestep)
+	def read_data(self, filename):
+		with open(filename) as f:
+			self._num_of_species = np.fromfile(f, dtype=np.int64, count=1)[0]
+			self._num_of_trans = np.fromfile(f, dtype=np.int64, count=self._num_of_species)
+			tot_num_of_trans = np.sum(self._num_of_trans, dtype=np.int64)
+			rectype = np.dtype( [ ('time', np.double), ('pops', np.double, tot_num_of_trans) ] )
+			#self._recs = np.memmap(f, dtype=rectype, mode='r')
+			self._recs = np.fromfile(f, dtype=rectype)
+	
+	def __init__(self, filename=None):
+		self._init()
+		if filename is not None:
+			self.read_data(filename)
 
-plt.plot(np.log10(recs['time'][1:]), np.log10(recs['pops'][1:,0]), label="level id = 1")
-plt.plot(np.log10(recs['time'][1:]), np.log10(recs['pops'][1:,2]), label="level id = 3")
+	def get_time(self):
+		return self._recs['time'][1:]
+
+	def get_data(self, ispec, itrans):
+		trans_to_begin = np.sum(self._num_of_trans[0:ispec], dtype=np.int64)
+		return self._recs['pops'][1:, trans_to_begin + itrans]
+
+def find_gradient(t, f):
+	res = []
+	time = []
+	for i in range(1, len(f)):
+		df_dt = (f[i] - f[i-1]) / (t[i] - t[i-1])
+		res.append(df_dt)
+		res.append(df_dt)
+		time.append(t[i-1])
+		time.append(t[i]*0.999)
+
+	return np.array(time), np.array(res)
+
+
+#recs = data_class(sys.argv[1])
+recs = data_class('pops_vs_time.bin')
+
+plt.plot(recs.get_time(), np.log10(recs.get_data(0, 0)), label="p-H2CO level id = 1")
+plt.plot(recs.get_time(), np.log10(recs.get_data(1, 2)), label="o-H2CO level id = 3")
 
 plt.legend()
 plt.show()
