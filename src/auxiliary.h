a4775ab66c5b87f6755df161965972a1976ab316
Parfenov Sergey <grey1105@yandex.ru>
2021-09-03 13:20:34 +0500

    Update auxiliary.h

--- src/auxiliary.h
+++ src/auxiliary.h
@@ -43,27 +43,3 @@ inline string trim(string & str)    // remove left and right spaces in a string
     str.erase(str.find_last_not_of(' ') + 1);       // surfixing spaces
     return str;
 }
-
-template <typename T>
-inline void two_sum(T & err, T & sum, const T & input)  // Knuth's Two-Sum algorithm
-{
-    const T a = sum;
-    const T b = input;
-
-    const T s = a + b;
-    const T a0 = s - b;
-    const T b0 = s - a0;
-    const T da = a - a0;
-    const T db = b - b0;
-    err = da + db;
-    sum = s;
-}
-
-template <typename T>
-inline T cascade_summation(T & err, T & sum, const T & input)   // cascaded summation algorithm of Rump, Ogita, and Oishi 
-{
-    T err_i = 0.0;
-    two_sum(err_i, sum, input);
-    err += err_i;
-    return sum;
-}
