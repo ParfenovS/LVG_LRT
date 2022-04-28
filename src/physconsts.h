764ee9750bcdf62020935723af292a6a9aab789d
Parfenov Sergey <grey1105@yandex.ru>
2022-04-29 01:28:11 +0500

    Update physconsts.h

--- src/physconsts.h
+++ src/physconsts.h
@@ -5,3 +5,4 @@ constexpr const double SPEED_OF_LIGHT = 299792458.0e2; // [cm/s]
 constexpr const double PLANK_CONSTANT = 6.626070040e-27; // [erg*s]
 constexpr const double BOLTZMANN_CONSTANT = 1.38064852e-16; // [erg/K]
 constexpr const double PI = 3.14159265358979323846;
+constexpr const double HC4PI = PLANK_CONSTANT * SPEED_OF_LIGHT / (4. * PI);
