184f7c29a9a8290e7ef5de0ad849d8dc4260a7b0
Parfenov Sergey <grey1105@yandex.ru>
2022-05-17 22:21:42 +0500

    Update physconsts.h

--- src/physconsts.h
+++ src/physconsts.h
@@ -4,5 +4,6 @@
 constexpr const double SPEED_OF_LIGHT = 299792458.0e2; // [cm/s]
 constexpr const double PLANK_CONSTANT = 6.626070040e-27; // [erg*s]
 constexpr const double BOLTZMANN_CONSTANT = 1.38064852e-16; // [erg/K]
+constexpr const double AMU = 1.66053904e-24;       // atomic mass unit [g]
 constexpr const double PI = 3.14159265358979323846;
 constexpr const double HC4PI = PLANK_CONSTANT * SPEED_OF_LIGHT / (4. * PI);
