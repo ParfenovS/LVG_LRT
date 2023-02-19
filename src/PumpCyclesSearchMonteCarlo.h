bf3b4f35519534269a8024941471efe59e8c4633
Parfenov Sergey <grey1105@yandex.ru>
2023-02-19 17:01:43 +0500

    Update PumpCyclesSearchMonteCarlo.h

--- src/PumpCyclesSearchMonteCarlo.h
+++ src/PumpCyclesSearchMonteCarlo.h
@@ -14,6 +14,7 @@ private:
 	struct cycle_class					// stores levels on a path from the i_lev to the k_lev levels
 	{
 		size_t cycle_counter = 1;
+		bool A_is_not_positive_for_final_level = false;
 		vector<size_t> lms;
 		vector<bool> CollDom;
 	};
@@ -42,27 +43,30 @@ private:
 		}
 
 		for (size_t i = 0; i < T.size(); i++) {
-			double sumT = 0.0, c = 0.0;
-			double y, t;
+			double sumT = 0.0;
+			//double y, t, c = 0.0;
 //#pragma omp parallel for reduction(+:sumT, c)			//parallelized Kahan summation of population flows (T)
 			for (size_t q = 0; q < T.size(); q++) {
-				y = fabs(T[i][q]) - c;
-				t = sumT + y;
-				c = (t - sumT) - y;
-				sumT = t;
+				/*if (T[i][q] > 0.0) {
+					y = T[i][q] - c;
+					t = sumT + y;
+					c = (t - sumT) - y;
+					sumT = t;	
+				}*/
+				if (T[i][q] > 0.0) sumT += T[i][q];
 			}
-			sumT = sumT - c;
+			//sumT = sumT - c;
 
-			sumT = 2. / sumT;
+			sumT = 1. / sumT;
 			for (size_t j = 0; j < T.size(); j++) {
 				if (T[i][j] <= 0.) P[i][j] = 0.0;
-				else P[i][j] = T[i][j] * sumT;				 
+				else P[i][j] = T[i][j] * sumT;
 			}
 		}
 		P[i_lev][k_lev] = 0.0;
 		P[i_lev][i_lev] = 0.0;
 		P0 = P;
-		MAX_STEPS = 1000 * P.size();
+		MAX_STEPS = 10 * P.size();
 	}
 
 	double compute_A(const size_t &lm)			// compute population flow from lm level to another levels
@@ -77,101 +81,104 @@ private:
 		for (size_t k = 0; k < P.size(); k++) P[k][lm] = 0.0;
 	}
 
-	size_t compare(const double &A, const double &R, const size_t &lm)		// searching for the next level (lm1) to transition to such that sum(q=1,lm1-1)Plmq < AR < sum(q=1,lm1)Plmq; the transition is from lm to lm1 levels;
+	size_t compare(const double &AR, const size_t &lm)		// searching for the next level (lm1) to transition to such that sum(q=1,lm1-1)Plmq < AR < sum(q=1,lm1)Plmq; the transition is from lm to lm1 levels;
 	{
 		double sumP1 = 0.0; 
 		double sumP2 = 0.0;
-		const double AR = A * R;
+		//const double AR = A * R;
 		for (size_t q = 0; q < P.size(); q++) {
-			sumP2 += P[lm][q];
-			if ( (sumP1 < AR && AR < sumP2) ) {		
-				return q;
+			if (P[lm][q] > 0.0) {
+				sumP2 += P[lm][q];
+				if (sumP1 <= AR && AR <= sumP2) {		
+					return q;
+				}
+				sumP1 = sumP2;
 			}
-			sumP1 = sumP2;
-			
 		}
 		return i_lev;
 	}
 
 	void search_for_cycles(mt19937 &gen, uniform_real_distribution<double> &dist)
 	{
-		size_t lm = i_lev;
-		size_t lm1, steps;
+		size_t lm, lm1, steps;
 		double A, R;
 		compute_P();
-		bool warning_on_final_level = false;
 		
-		if (compute_A(i_lev) <= 0.0) throw runtime_error("A=0 for initial level, it's impossible to find a cycle");
+		if (compute_A(i_lev) <= 0.0) throw runtime_error("A<=0 for the initial level, it's impossible to find a cycle");
 
 		for (size_t c = 0; c < num_of_tries; c++) {
 			steps = 0;
 			cycle_class cycle;
 			lm = i_lev;
-			lm1 = lm;
 			P = P0;
 			A = compute_A(lm);
-			while (A > 0.0 && steps < MAX_STEPS) {
-				cycle.lms.push_back(lm);
-				R = dist(gen);							// generating random R
-				lm1 = compare(A, R, lm); 				// searching for the next level (lm1) such that sum(q=1,lm1-1)Plmq < AR < sum(q=1,lm1)Plmq; the transition is from lm to lm1 levels
-				if (close_levels) close_level(lm);		// closing the current level
-				cycle.CollDom.push_back(isItCollisionalDominated[lm][lm1]);
-				if (lm == k_lev) break;
-				lm = lm1;
+			cycle.lms.push_back(lm);
+
+			while (true) {
 				steps += 1;
-				A = compute_A(lm);
-				if (A <= 0.0) {							// the path was not successfull
-					cycle.CollDom.clear();
-					cycle.lms.clear();
-					if (lm == k_lev && !warning_on_final_level) {
-						cout << "# warning: A<=0 for the final level; maybe one need to consider another final level\n";
-						warning_on_final_level = true;
+				R = dist(gen);							// generating random R
+				lm1 = compare(A*R, lm); 				// searching for the next level (lm1) such that sum(q=1,lm1-1)Plmq < AR < sum(q=1,lm1)Plmq; the transition is from lm to lm1 levels
+				double Ai = compute_A(lm1);
+				if (Ai <= 0.0 && lm1 != k_lev) {
+					if (steps >= MAX_STEPS) {
+						steps = 0;
+						cycle.lms.clear();
+						cycle.CollDom.clear();
+						lm = i_lev;
+						cycle.lms.push_back(lm);
+						P = P0;
+						A = compute_A(lm);
 					}
-					P = P0;
-					lm = i_lev;
-					A = compute_A(lm);
-				}
-			}
-			if (steps == MAX_STEPS)	cerr << "# warning: Maximum number of steps in pop_flow() has been reached\n";
-			if (steps != MAX_STEPS && lm == k_lev) {
-				if (cycles.size() == 0) {
-					cycles.push_back(cycle);
-					cycle.CollDom.clear();
-					cycle.lms.clear();
 					continue;
 				}
-				size_t same_cycle_id = 0;
-				bool same_cycle = false;
-
-				for (size_t n = 0; n < cycles.size(); n++) {		// check whether the cycle has been found at previous attempts
-					if (cycle.lms.size() == cycles[n].lms.size()) {
-						same_cycle_id = n;
-						same_cycle = true;
-						for (size_t m = 0; m < cycles[n].lms.size(); m++) {
-							if (cycles[n].lms[m] != cycle.lms[m]) {
-								same_cycle = false;
-								break;
-							}
-						}
-						if (same_cycle) break;
-					}
+				if (close_levels) close_level(lm);		// closing the current level
+				cycle.CollDom.push_back(isItCollisionalDominated[lm][lm1]);
+				cycle.lms.push_back(lm1);
+				lm = lm1;
+				if (close_levels) A = compute_A(lm);
+				else A = Ai;
+				if (lm == k_lev) {
+					if (A <= 0.0) cycle.A_is_not_positive_for_final_level = true;
+					steps = 0;
+					break;
 				}
-				if (!same_cycle) cycles.push_back(cycle);
-				else cycles[same_cycle_id].cycle_counter += 1;
 			}
+
+			if (cycles.size() == 0) {
+				cycles.push_back(cycle);
+				cycle.CollDom.clear();
+				cycle.lms.clear();
+				continue;
+			}
+			size_t same_cycle_id = 0;
+			bool same_cycle = false;
+			for (size_t n = 0; n < cycles.size(); n++) {		// check whether the cycle has been found at previous attempts
+				if (cycle.lms.size() == cycles[n].lms.size()) {
+					same_cycle_id = n;
+					same_cycle = true;
+					for (size_t m = 0; m < cycles[n].lms.size(); m++) {
+						if (cycles[n].lms[m] != cycle.lms[m]) {
+							same_cycle = false;
+							break;
+						}
+					}
+					if (same_cycle) break;
+				}
+			}
+			if (!same_cycle) cycles.push_back(cycle);
+			else cycles[same_cycle_id].cycle_counter += 1;
 			cycle.CollDom.clear();
 			cycle.lms.clear();
 		}
 
 		// calculate the relative efficiency and power of cycles
 		vector <double> fE(cycles.size()); // relative efficiency
-		vector <double> fW(cycles.size());  // power
+		vector <double> fW(cycles.size()); // power
 
 		double sum_T = 0.0;
 		for (size_t q = 0; q < T.size(); q++) {
-			sum_T += fabs(T[i_lev][q]);
+			if (T[i_lev][q] > 0.0) sum_T += T[i_lev][q];
 		}
-		sum_T *= 0.5;
 
 		for (size_t c = 0; c < cycles.size(); c++)
 		{
@@ -191,7 +198,9 @@ private:
 		{
 			size_t c = sort_indexes[ic];
 			if (fabs(fE[c]) < 1.e-30) continue;
-			cout << cycles[c].lms.size() << " " << cycles[c].cycle_counter << " " << fE[c] << " " << fW[c] << "\n";
+			cout << cycles[c].lms.size() << " " << cycles[c].cycle_counter << " " << fE[c] << " " << fW[c];
+			if (cycles[c].A_is_not_positive_for_final_level) cout << " # warning A<=0 for this route";
+			cout << "\n";
 
 			for (size_t m = 0; m < cycles[c].lms.size(); m++)
 			{
@@ -212,7 +221,7 @@ public:
 		this->i_lev = i - 1; 	// convert to zero-based indexes
 		this->k_lev = k - 1; 	// convert to zero-based indexes
 		mt19937 gen(seed);
-		uniform_real_distribution<double> dist(0, 1.0f - 2.0f*DBL_EPSILON);
+		uniform_real_distribution<double> dist(0, 1.0f);
 		search_for_cycles(gen, dist);
 	}
 	
